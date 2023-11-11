d57d60ad6a5c2248fb0edd00f6d7cb05c6fa8671
Mohamed <mootrz16@gmail.com>
2023-11-11 06:34:21 +0200

    ios dominoesss

--- android/app/src/main/kotlin/com/example/project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/project/MainActivity.kt
@@ -1,6 +1,8 @@
-package com.example.project
+package io.flutter.plugins.firebasecoreexample
 
-import io.flutter.embedding.android.FlutterActivity
+import com.example.social_app.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity :
+    FlutterActivity() { // You can keep this empty class or remove it. Plugins on the new embedding
+    // now automatically registers plugins.
 }
